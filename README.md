711ceae82c7a7f8ff034ba53b403fbd587d7474d
James Allred <wildj79@gmail.com>
2020-01-17 08:58:34 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Market Square
+
+A simple module that allows you to setup shops and stores that your PC's can buy items from.
---
+++ dist/module/preloadTemplates.js
@@ -0,0 +1,7 @@
+export const preloadTemplates = async function() {
+	const templatePaths = [
+		// Add paths to "modules/market-square/templates"
+	];
+
+	return loadTemplates(templatePaths);
+}
