2dbd90aaef894855ae8408cabd38419adfed8028
James Allred <wildj79@gmail.com>
2020-01-17 13:07:10 -0600

    Preping my environment for development.

--- dist/market-square.js
+++ dist/market-square.js
@@ -1,13 +1,9 @@
+/// <reference path="../node_modules/foundry-pc-types/index.d.ts" />
 /**
- * This is your JavaScript entry file for Foundry VTT.
- * Register custom settings, sheets, and constants using the Foundry API.
- * Change this heading to be more descriptive to your module, or remove it.
- * Author: [your name]
- * Content License: [copyright and-or license] If using an existing system
- * 					you may want to put a (link to a) license or copyright
- * 					notice here (e.g. the OGL).
- * Software License: [your license] Put your desired license here, which
- * 					 determines how others may use and modify your module
+ * Initializes the module.
+ * 
+ * Author: wildj79
+ * Software License: MIT
  */
 
 // Import JavaScript modules
@@ -18,7 +14,7 @@ import { preloadTemplates } from './module/preloadTemplates.js';
 /* Initialize module					*/
 /* ------------------------------------ */
 Hooks.once('init', async function() {
-	console.log('market-square | Initializing market-square');
+	console.log('Market Square | Initializing markets');
 
 	// Assign custom classes and constants here
 	
---
+++ dist/module/sheet.js
@@ -0,0 +1,3 @@
+export class ActorSheetMarket extends ActorSheet {
+
+}
